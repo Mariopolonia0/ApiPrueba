97ba0d3471b0a30ccc2b7fa904a87df9e84237e6
Mario <Mario@DESKTOP-MARIO>
2021-10-30 20:34:28 -0400

    implemente el adapter

--- app/src/main/java/com/example/apiprueva/MainActivity.kt
+++ app/src/main/java/com/example/apiprueva/MainActivity.kt
@@ -3,25 +3,26 @@ package com.example.apiprueva
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.lifecycleScope
+import com.example.apiprueva.databinding.ActivityMainBinding
 import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
-    private var _binding : MainActivity? = null
-    private val binding get() = _binding
 
-    //private lateinit var viewModel
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       lifecycleScope.launchWhenCreated {
-           val lista = RetrofitInstance.api.GetTravels()
-           val cant = lista.size
+        lifecycleScope.launchWhenCreated {
+            val lista = RetrofitInstance.api.GetTravels()
+            //val cant = lista.size
+
+            var adapter = TravelAdapter()
+            adapter.sumitList(lista)
+            binding.listatravelsrecyclerview.adapter = adapter
 
        }
-
-
     }
 }
